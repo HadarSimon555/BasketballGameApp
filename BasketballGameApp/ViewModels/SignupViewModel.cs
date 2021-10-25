0857c87c95d755b4013a8221657fcc2d326d3dbc
User <User@LAB2-2>
2021-10-25 13:08:07 +0300

    signup

--- BasketballGameApp/ViewModels/SignupViewModel.cs
+++ BasketballGameApp/ViewModels/SignupViewModel.cs
@@ -1,6 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+using BasketballGameApp.Services;
+using BasketballGameApp.Models;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Xamarin.Essentials;
 
 namespace BasketballGameApp.ViewModels
 {
@@ -54,8 +64,8 @@ namespace BasketballGameApp.ViewModels
         #endregion
 
         #region BirthDate
-        private string birthDate;
-        public string BirthDate
+        private DateTime birthDate;
+        public DateTime BirthDate
         {
             get => birthDate;
             set
@@ -66,9 +76,9 @@ namespace BasketballGameApp.ViewModels
             }
         }
 
-        private string birthDateError;
+        private DateTime birthDateError;
 
-        public string BirthDateError
+        public DateTime BirthDateError
         {
             get => birthDateError;
             set
@@ -80,7 +90,7 @@ namespace BasketballGameApp.ViewModels
 
         private void ValidateBirthDate()
         {
-            this.ShowBirthDateError = string.IsNullOrEmpty(BirthDate);
+            this.ShowBirthDateError = DateTime.IsNullOrEmpty(BirthDate);
         }
 
         private bool showBirthDateError;
@@ -95,7 +105,7 @@ namespace BasketballGameApp.ViewModels
         }
         #endregion
 
-        #region מקור התמונה
+        #region Image
         private string imgSrc;
 
         public string ImgSrc
@@ -108,6 +118,269 @@ namespace BasketballGameApp.ViewModels
             }
         }
         private const string DEFAULT_PHOTO_SRC = "defaultphoto.jpg";
+
+        ///The following command handle the pick photo button
+        FileResult imageFileResult;
+        public event Action<ImageSource> SetImageSourceEvent;
+        public ICommand PickImageCommand => new Command(OnPickImage);
+        public async void OnPickImage()
+        {
+            FileResult result = await MediaPicker.PickPhotoAsync(new MediaPickerOptions()
+            {
+                Title = "בחר תמונה"
+            });
+
+            if (result != null)
+            {
+                this.imageFileResult = result;
+
+                var stream = await result.OpenReadAsync();
+                ImageSource imgSource = ImageSource.FromStream(() => stream);
+                if (SetImageSourceEvent != null)
+                    SetImageSourceEvent(imgSource);
+            }
+        }
+
+        ///The following command handle the take photo button
+        public ICommand CameraImageCommand => new Command(OnCameraImage);
+        public async void OnCameraImage()
+        {
+            var result = await MediaPicker.CapturePhotoAsync(new MediaPickerOptions()
+            {
+                Title = "צלם תמונה"
+            });
+
+            if (result != null)
+            {
+                this.imageFileResult = result;
+                var stream = await result.OpenReadAsync();
+                ImageSource imgSource = ImageSource.FromStream(() => stream);
+                if (SetImageSourceEvent != null)
+                    SetImageSourceEvent(imgSource);
+            }
+        }
+        #endregion
+
+        #region Height
+        private double height;
+        public double Height
+        {
+            get => height;
+            set
+            {
+                height = value;
+                ValidateHeight();
+                OnPropertyChanged("Height");
+            }
+        }
+
+        private double heightError;
+
+        public double HeightError
+        {
+            get => heightError;
+            set
+            {
+                heightError = value;
+                OnPropertyChanged("HeightError");
+            }
+        }
+
+        private void ValidateHeight()
+        {
+            this.ShowHeightError = double.IsNullOrEmpty(Height);
+        }
+
+        private bool showHeightError;
+        public bool ShowHeightError
+        {
+            get => showHeightError;
+            set
+            {
+                showHeightError = value;
+                OnPropertyChanged("ShowHeightError");
+            }
+        }
+        #endregion
+
+        #region Gender
+        private string gender;
+        public string Gender
+        {
+            get => gender;
+            set
+            {
+                gender = value;
+                ValidateGender();
+                OnPropertyChanged("Gender");
+            }
+        }
+
+        private string genderError;
+
+        public string GenderError
+        {
+            get => genderError;
+            set
+            {
+                genderError = value;
+                OnPropertyChanged("GenderError");
+            }
+        }
+
+        private void ValidateGender()
+        {
+            this.ShowGenderError = string.IsNullOrEmpty(Gender);
+        }
+
+        private bool showGenderError;
+        public bool ShowGenderError
+        {
+            get => showGenderError;
+            set
+            {
+                showGenderError = value;
+                OnPropertyChanged("ShowGenderError");
+            }
+        }
+        #endregion
+
+        #region City
+        private string city;
+        public string City
+        {
+            get => city;
+            set
+            {
+                city = value;
+                ValidateCity();
+                OnPropertyChanged("City");
+            }
+        }
+
+        private string cityError;
+
+        public string CityError
+        {
+            get => cityError;
+            set
+            {
+                cityError = value;
+                OnPropertyChanged("CityError");
+            }
+        }
+
+        private void ValidateCity()
+        {
+            this.ShowCityError = string.IsNullOrEmpty(City);
+        }
+
+        private bool showCityError;
+        public bool ShowCityError
+        {
+            get => showCityError;
+            set
+            {
+                showCityError = value;
+                OnPropertyChanged("ShowCityError");
+            }
+        }
+        #endregion
+
+        #region Email
+        private bool showEmailError;
+
+        public bool ShowEmailError
+        {
+            get => showEmailError;
+            set
+            {
+                showEmailError = value;
+                OnPropertyChanged("ShowEmailError");
+            }
+        }
+
+        private string email;
+
+        public string Email
+        {
+            get => email;
+            set
+            {
+                email = value;
+                ValidateEmail();
+                OnPropertyChanged("Email");
+            }
+        }
+
+        private string emailError;
+
+        public string EmailError
+        {
+            get => emailError;
+            set
+            {
+                emailError = value;
+                OnPropertyChanged("EmailError");
+            }
+        }
+
+        private void ValidateEmail()
+        {
+            this.ShowEmailError = string.IsNullOrEmpty(Email);
+            if (!this.ShowEmailError)
+            {
+                if (!Regex.IsMatch(this.Email, @"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$"))
+                {
+                    this.ShowEmailError = true;
+                    this.EmailError = ERROR_MESSAGES.BAD_EMAIL;
+                }
+            }
+            else
+                this.EmailError = ERROR_MESSAGES.REQUIRED_FIELD;
+        }
+        #endregion
+
+        #region Password
+        private string pass;
+        public string Pass
+        {
+            get => pass;
+            set
+            {
+                pass = value;
+                ValidatePass();
+                OnPropertyChanged("Pass");
+            }
+        }
+
+        private string passError;
+
+        public string PassError
+        {
+            get => passError;
+            set
+            {
+                passError = value;
+                OnPropertyChanged("PassError");
+            }
+        }
+
+        private void ValidatePass()
+        {
+            this.ShowPassError = string.IsNullOrEmpty(Pass);
+        }
+
+        private bool showPassError;
+        public bool ShowPassError
+        {
+            get => showPassError;
+            set
+            {
+                showPassError = value;
+                OnPropertyChanged("ShowPassError");
+            }
+        }
         #endregion
     }
 }
