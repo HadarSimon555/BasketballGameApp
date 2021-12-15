e09cf34d93549e7b6762ce75647319799097aefe
User <User@LAB2-2>
2021-12-15 12:51:15 +0200

    15/12

--- BasketballGameApp/ViewModels/PlayerCardViewModel.cs
+++ BasketballGameApp/ViewModels/PlayerCardViewModel.cs
@@ -1,11 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+using BasketballGameApp.Services;
+using BasketballGameApp.Models;
+using System.Collections.ObjectModel;
+using System.Linq;
+using BasketballGameApp.Views;
 
 namespace BasketballGameApp.ViewModels
 {
     class PlayerCardViewModel : BaseViewModel
     {
+        #region Constructor
+        public PlayerCardViewModel()
+        {
+            currentPlayer = new PlayerOnTeamForSeason();
+            //add server status page
+            LoadPlayer();
+        }
+        #endregion
 
+        private PlayerOnTeamForSeason currentPlayer;
+        public PlayerOnTeamForSeason CurrentPlayer
+        {
+            get
+            {
+                return this.currentPlayer;
+            }
+            set
+            {
+                if (this.currentPlayer != value)
+                {
+
+                    this.currentPlayer = value;
+                    OnPropertyChanged("CurrentPlayer");
+                }
+            }
+        }
+
+        public async void LoadPlayer()
+        {
+            BasketballGameAPIProxy proxy = BasketballGameAPIProxy.CreateProxy();
+            App cur = (App)Application.Current;
+            currentPlayer = await proxy.GetPlayerOnTeamForSeasonAsync(cur.CurrentUser);
+        }
     }
 }
