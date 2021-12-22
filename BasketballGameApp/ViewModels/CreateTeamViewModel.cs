10ce0132263fa5a0a56af060e95ae0907b09cbbb
HadarSimon555 <77894680+HadarSimon555@users.noreply.github.com>
2021-12-22 15:05:24 +0200

    22/12

--- BasketballGameApp/ViewModels/CreateTeamViewModel.cs
+++ BasketballGameApp/ViewModels/CreateTeamViewModel.cs
@@ -1,10 +1,97 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+using BasketballGameApp.Services;
+using BasketballGameApp.Models;
+using BasketballGameApp.Views;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Xamarin.Essentials;
 
 namespace BasketballGameApp.ViewModels
 {
     class CreateTeamViewModel : BaseViewModel
     {
+        #region Name
+        private string name;
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                ValidateName();
+                OnPropertyChanged("Name");
+            }
+        }
+
+        private string nameError;
+
+        public string NameError
+        {
+            get => nameError;
+            set
+            {
+                nameError = value;
+                OnPropertyChanged("NameError");
+            }
+        }
+
+        private void ValidateName()
+        {
+            this.ShowNameError = string.IsNullOrEmpty(Name);
+        }
+
+        private bool showNameError;
+        public bool ShowNameError
+        {
+            get => showNameError;
+            set
+            {
+                showNameError = value;
+                OnPropertyChanged("ShowNameError");
+            }
+        }
+        #endregion
+
+        #region TeamImgSrc
+        private string teamImgSrc;
+
+        public string TeamImgSrc
+        {
+            get => teamImgSrc;
+            set
+            {
+                teamImgSrc = value;
+                OnPropertyChanged("TeamImgSrc");
+            }
+        }
+        private const string DEFAULT_PHOTO_SRC = "defaultphoto.png";
+        #endregion
+
+        #region IsCoach
+        private bool isCoach;
+        public bool IsCoach
+        {
+            get => isCoach;
+            set
+            {
+                isCoach = value;
+                OnPropertyChanged("IsCoach");
+            }
+        }
+        #endregion
+
+        #region Constructor
+        public CreateTeamViewModel()
+        {
+            
+        }
+        #endregion
     }
 }
--- BasketballGameApp/Views/CreateTeam.xaml.cs
+++ BasketballGameApp/Views/CreateTeam.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BasketballGameApp.ViewModels;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -14,6 +15,8 @@ namespace BasketballGameApp.Views
     {
         public CreateTeam()
         {
+            CreateTeamViewModel vm = new CreateTeamViewModel();
+            this.BindingContext = vm;
             InitializeComponent();
         }
     }
