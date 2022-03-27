6abb12e8dcad8d640a8fe413101e08fadff2bc3e
HadarSimon555 <77894680+HadarSimon555@users.noreply.github.com>
2022-03-27 21:17:33 +0300

    27/03

--- BasketballGameApp/ViewModels/LogOutViewModel.cs
+++ BasketballGameApp/ViewModels/LogOutViewModel.cs
@@ -1,11 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+using BasketballGameApp.Services;
+using BasketballGameApp.Models;
+using BasketballGameApp.Views;
+using Xamarin.Essentials;
+using System.Linq;
+using BasketballGameApp.DTO;
 
 namespace BasketballGameApp.ViewModels
 {
     class LogOutViewModel : BaseViewModel
     {
+        #region Constructor
+        public LogOutViewModel()
+        {
+            SubmitCommand = new Command(OnSubmit);
+        }
+        #endregion
 
+        #region ServerStatus
+        private string serverStatus;
+        public string ServerStatus
+        {
+            get { return serverStatus; }
+            set
+            {
+                serverStatus = value;
+                OnPropertyChanged("ServerStatus");
+            }
+        }
+        #endregion
+
+        #region OnSubmit
+        public ICommand SubmitCommand { protected set; get; }
+        public async void OnSubmit()
+        {
+            ServerStatus = "מתחבר לשרת...";
+            await App.Current.MainPage.Navigation.PushModalAsync(new Views.ServerStatusPage(this));
+
+            BasketballGameAPIProxy proxy = BasketballGameAPIProxy.CreateProxy();
+
+            ServerStatus = "קורא נתונים...";
+            App theApp = (App)App.Current;
+            theApp.CurrentUser = null;
+            theApp.CurrentPlayer = null;
+            theApp.CurrentCoach = null;
+
+            await App.Current.MainPage.DisplayAlert("התנתקות", "ההתנתקות בוצעה בהצלחה", "אישור", FlowDirection.RightToLeft);
+            await App.Current.MainPage.Navigation.PopModalAsync();
+            NavigationPage p = new NavigationPage(new GamesScores());
+            NavigationPage.SetHasNavigationBar(p, false);
+            await App.Current.MainPage.Navigation.PushAsync(p);
+
+        }
+        #endregion
     }
 }
--- BasketballGameApp/Views/LogOut.xaml.cs
+++ BasketballGameApp/Views/LogOut.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using BasketballGameApp.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -14,6 +14,8 @@ namespace BasketballGameApp.Views
     {
         public LogOut()
         {
+            LogOutViewModel context = new LogOutViewModel();
+            this.BindingContext = context;
             InitializeComponent();
         }
     }
